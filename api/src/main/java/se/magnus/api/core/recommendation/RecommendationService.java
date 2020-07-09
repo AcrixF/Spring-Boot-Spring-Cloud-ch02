8c22f15db5a62a6eaac0a3d8f09fb135fda3d9d9
Cristian Flores <Cristian.X.Flores.-ND@disney.com>
2020-07-09 17:18:05 -0500

    Composite Service

--- api/src/main/java/se/magnus/api/core/recommendation/RecommendationService.java
+++ api/src/main/java/se/magnus/api/core/recommendation/RecommendationService.java
@@ -1,4 +1,15 @@
 package se.magnus.api.core.recommendation;
 
-public class RecommendationService {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+public interface RecommendationService {
+
+    @GetMapping(
+            value = "/recommendation",
+            produces = "application/json")
+    List<Recommendation> getRecommendations(@RequestParam(value = "productId", required = true) int productId);
+
 }
